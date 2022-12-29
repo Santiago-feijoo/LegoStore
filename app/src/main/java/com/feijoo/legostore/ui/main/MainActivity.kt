28ed27ae7b9fce33a240a19f9bcbbc62dc5a9264
Santiago Feijoo <49215448+Santiago-feijoo@users.noreply.github.com>
2022-12-28 20:05:45 -0500

    Uploading project

---
+++ app/src/main/java/com/feijoo/legostore/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.feijoo.legostore.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feijoo.legostore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
