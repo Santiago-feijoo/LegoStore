1d8ec6e448819f0c21ecb55a561a32a9f3ff3320
Santiago Feijoo <49215448+Santiago-feijoo@users.noreply.github.com>
2022-12-28 23:36:12 -0500

    Improvements

--- app/src/main/java/com/feijoo/legostore/ui/main/MainActivity.kt
+++ app/src/main/java/com/feijoo/legostore/ui/main/MainActivity.kt
@@ -2,11 +2,35 @@ package com.feijoo.legostore.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.feijoo.legostore.BuildConfig
 import com.feijoo.legostore.R
+import com.feijoo.legostore.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+    /** Attributes **/
+    private lateinit var binding: ActivityMainBinding
+
+    /** Methods **/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initComponents()
+
     }
+
+    private fun initComponents() {
+        binding.textViewVersion.text = getString(R.string.version, BuildConfig.VERSION_NAME)
+
+        initListener()
+
+    }
+
+    private fun initListener() {
+
+
+    }
+
 }
